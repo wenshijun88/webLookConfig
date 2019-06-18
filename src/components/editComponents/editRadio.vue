0b4627c41eeba6a3389c92f68e851c850f79811f
wenshijun <wsj745831>
2019-06-18 17:21:29 +0800

    fix：初步添加右侧编辑组件功能

---
+++ src/components/editComponents/editRadio.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="component-container">
+        <el-form :model="editComponentForm">
+            <el-form-item label="文本">
+                <el-input v-model="editComponentForm.text"></el-input>
+            </el-form-item>
+        </el-form>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'editRadio',
+        data(){
+            return {
+                editComponentForm: {
+                    text: ''
+                }
+            }
+        }
+    }
+</script>
--- src/views/editView.vue
+++ src/views/editView.vue
@@ -1,6 +1,18 @@
 <template>
     <div class="container">
-        <component :is="item.componentName" v-for="item in componentList"/>
+        <div class="view-container">
+            <div v-for="item in componentList">
+                <component :is="item.componentName" @click.native="showComponentEdit(item)"/>
+                <span>{{item.innerText}}</span>
+            </div>
+        </div>
+        <div class="edit-container">
+            <el-form v-if="editFormData[curComponentId]">
+                <el-form-item label="文本" prop="innerText">
+                    <el-input v-model="editFormData[curComponentId].innerText"></el-input>
+                </el-form-item>
+            </el-form>
+        </div>
     </div>
 </template>
 
@@ -8,7 +20,10 @@
     export default {
         name: 'editView',
         data(){
-            return {}
+            return {
+                editFormData: {},
+                curComponentId: '', //当前编辑的组件id
+            }
         },
         mounted(){
 
@@ -17,6 +32,28 @@
             componentList(){
                 return this.$store.state.viewConfig.saveComponent
             }
+        },
+        methods: {
+            showComponentEdit(item){
+                this.curComponentId = item.menuId;
+                if(!this.editFormData[item.menuId]){
+                    this.$set(this.editFormData,item.menuId,item)
+                }
+            }
         }
     }
 </script>
+<style lang="less">
+    .container{
+        width: 100%;
+        display: flex;
+        justify-content: space-between;
+        .el-form-item{
+            display: flex;
+            align-items: center;
+        }
+    }
+    .el-radio{
+        margin-right: 0;
+    }
+</style>
